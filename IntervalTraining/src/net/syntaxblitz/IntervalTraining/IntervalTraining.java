53bd7c85302c403c2311800a5ea5d32206e19585
Timothy Aveni <timothy@tacosareawesome.com>
2014-09-15 21:06:39 -0400

    Add TODO regarding enharmonic spelling selection

--- IntervalTraining/src/net/syntaxblitz/IntervalTraining/IntervalTraining.java
+++ IntervalTraining/src/net/syntaxblitz/IntervalTraining/IntervalTraining.java
@@ -58,7 +58,7 @@ public class IntervalTraining {
 		System.exit(0);
 	}
 
-	private String getNoteName(int halfStepsFromA) {
+	private String getNoteName(int halfStepsFromA) {	// TODO: make context-sensitive: should show flat/sharp depending on context
 		while (halfStepsFromA < 0) {
 			halfStepsFromA += 12;
 		}
